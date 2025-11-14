b4ba27c287c28319564fd0a27a3fad64799990ed
fds86 <francescodalsanto@gmail.com>
2025-11-14 22:00:12 +0100

    Compilato e testato esercizio 3_2_5

---
+++ lezione_03/Esercizio_3_2_5.java
@@ -0,0 +1,27 @@
+
+public class Esercizio_3_2_5 {
+
+	public static void main(String[] args) {
+		int x = 7;
+		switch (x) {
+		case 1: 
+			System.out.println("dire"); 
+			break;
+		case 2: 
+			System.out.println("fare"); 
+			break;
+		case 3: 
+			System.out.println("baciare"); 
+			break;
+		case 4: 
+			System.out.println("lettera"); 
+			break;
+		case 5: 
+			System.out.println("testamento"); 
+			break;
+		default: 
+			System.out.println("non so...");
+			break;
+		}
+	}
+}
